15989fccd612cd7b95ec57d087ca31a828cc82a9
AkiEvka <evanedeliakova@gmail.com>
2023-03-30 17:33:56 +0200

    test connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, Response, abort
 import os
+import kfp
 
 app = Flask(__name__)
 
@@ -9,18 +10,14 @@ def index_action():
 
 @app.route('/list')
 def list_action():
-    import kfp
+#     client = kfp.Client(host="http://ml-pipeline-ui-artifact:80")
+#
+#     print(client.list_experiments())
+#     sleep(90000000000)
 
-    # the namespace in which you deployed Kubeflow Pipelines
-
-    # the KF_PIPELINES_SA_TOKEN_PATH environment variable is used when no `path` is set
-    # the default KF_PIPELINES_SA_TOKEN_PATH is /var/run/secrets/kubeflow/pipelines/token
-#     credentials = kfp.auth.ServiceAccountTokenVolumeCredentials(path=None)
-
+if __name__ == '__main__':
     client = kfp.Client(host="http://ml-pipeline-ui-artifact:80")
 
     print(client.list_experiments())
     sleep(90000000000)
-
-if __name__ == '__main__':
     app.run()
