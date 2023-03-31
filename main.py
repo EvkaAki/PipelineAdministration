85683b58d1bc46949f4ca3a5d7f25cdad0e0ae0f
AkiEvka <evanedeliakova@gmail.com>
2023-03-31 13:38:25 +0200

    fiox

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, Response, abort
 import os
 import kfp
+import time
+
 
 app = Flask(__name__)
 
@@ -20,5 +22,5 @@ def list_action():
 if __name__ == '__main__':
 #     client = kfp.Client(host="http://ml-pipeline:8888")
 #     print(client.list_experiments())
-    sleep(90000000000)
+    time.sleep(90000000000)
     app.run()
