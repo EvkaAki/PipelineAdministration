176dd6935a655fdb6975738dc55526cbaeb78d7e
AkiEvka <evanedeliakova@gmail.com>
2023-03-31 16:11:59 +0200

    list experiments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, Response, abort
 import os
-import requests
+import kfp
 
 
 app = Flask(__name__)
@@ -12,14 +12,24 @@ def index_action():
 @app.route('/list')
 def list_action():
     print('list')
+#     client = kfp.Client(host="http://ml-pipeline.kubeflow.svc.cluster.local:8888")
+#
+#     print(client.list_experiments())
+#     sleep(90000000000)
 
 
 if __name__ == '__main__':
-    response = requests.get("http://ml-pipeline:8888/apis/v1beta1/healthz")
-    print(response)
-    print(response.json())
-    params = {'resource_reference_key.type':'NAMESPACE', 'resource_reference_key.id' : 'admin'}
-    response = requests.get("http://ml-pipeline:8888/apis/v1beta1/experiments", params=params)
-    print(response)
-    print(response.json())
+#     response = requests.get("http://ml-pipeline:8888/apis/v1beta1/healthz")
+#     print(response)
+#     print(response.json())
+#     params = {'resource_reference_key.type':'NAMESPACE', 'resource_reference_key.id' : 'admin'}
+#     response = requests.get("http://ml-pipeline:8888/apis/v1beta1/experiments", params=params)
+#     print(response)
+#     print(response.json())
+#     app.run()
+
+    with open(os.environ['KF_PIPELINES_SA_TOKEN_PATH'], "r") as f:
+        TOKEN = f.read()
+    client = kfp.Client(host="http://ml-pipeline.kubeflow.svc.cluster.local:8888")
+    print(client.list_experiments())
     app.run()
