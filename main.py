45d2acaa41280910e965af9b89e4c4f0e263cf7f
AkiEvka <evanedeliakova@gmail.com>
2023-04-12 09:51:18 +0200

    list experimnets in admin namespace

--- main.py
+++ main.py
@@ -21,6 +21,6 @@ def list_action():
 if __name__ == '__main__':
     credentials = kfp.auth.ServiceAccountTokenVolumeCredentials(path=None)
     client = kfp.Client(host="http://ml-pipeline.kubeflow.svc.cluster.local:8888", credentials=credentials)
-    print(client.list_experiments(namespace=admin))
+    print(client.list_experiments(namespace="admin"))
     app.run()
 
