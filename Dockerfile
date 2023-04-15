b8788e825ca809b1e816c1c3c87f8328b0169b46
AkiEvka <evanedeliakova@gmail.com>
2023-04-15 15:57:38 +0200

    edit error

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,16 @@
-FROM python:3.10-alpine
+FROM python:3.7 AS backend-kubeflow-wheel
+
+WORKDIR /src
+COPY ./components/crud-web-apps/common/backend .
+
+RUN python3 setup.py bdist_wheel
+
+FROM python:3.7
+
+WORKDIR /package
+COPY --from=backend-kubeflow-wheel /src .
+RUN pip3 install .
+
 WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
--- main.py
+++ main.py
@@ -19,19 +19,8 @@ def list_action():
 
 
 if __name__ == '__main__':
-#     response = requests.get("http://ml-pipeline:8888/apis/v1beta1/healthz")
-#     print(response)
-#     print(response.json())
-#     params = {'resource_reference_key.type':'NAMESPACE', 'resource_reference_key.id' : 'admin'}
-#     response = requests.get("http://ml-pipeline:8888/apis/v1beta1/experiments", params=params)
-#     print(response)
-#     print(response.json())
-#     app.run()
-    f = open("/var/run/secrets/kubernetes.io/serviceaccount/token", "r")
-    token = f.readline()
-    print(token)
     credentials = kfp.auth.ServiceAccountTokenVolumeCredentials(path=None)
     client = kfp.Client(host="http://ml-pipeline.kubeflow.svc.cluster.local:8888", credentials=credentials)
-    print(client.list_experiments())
+    print(client.list_experiments(namespace="admin"))
     app.run()
 
