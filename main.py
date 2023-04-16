82f5beeee3a24329dabbe3ff00222ed7f6b4f176
AkiEvka <evanedeliakova@gmail.com>
2023-04-16 21:23:03 +0200

    add logging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, Response, abort
 import os
 import kfp
+import pprint
 
 
 app = Flask(__name__)
@@ -20,4 +21,19 @@ if __name__ == '__main__':
     client = kfp.Client(host="http://ml-pipeline.kubeflow.svc.cluster.local:8888", credentials=credentials)
     print(client.list_experiments(namespace="admin"))
     app.run(host="localhost", port=3500, debug=True)
+
+
+class LoggingMiddleware(object):
+    def __init__(self, app):
+        self._app = app
+
+    def __call__(self, env, resp):
+        errorlog = env['wsgi.errors']
+        pprint.pprint(('REQUEST', env), stream=errorlog)
+
+        def log_response(status, headers, *args):
+            pprint.pprint(('RESPONSE', status, headers), stream=errorlog)
+            return resp(status, headers, *args)
+
+        return self._app(env, log_response)
 
