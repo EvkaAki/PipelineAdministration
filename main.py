7dec961564225b6793fbc55b615af798501e2e5f
AkiEvka <evanedeliakova@gmail.com>
2023-04-27 17:07:25 +0200

    added arguments

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def researcher_action():
     credentials = kfp.auth.ServiceAccountTokenVolumeCredentials(path=None)
     client = kfp.Client(host="http://ml-pipeline.kubeflow.svc.cluster.local:8888", credentials=credentials)
     namespace = client.get_user_namespace()
-    pipelines = client.list_pipelines(namespace=namespace)
+    pipelines = client.list_pipelines('', 30, None, str(namespace)
     # return render_template('index.html')
     return str(pipelines)
 
